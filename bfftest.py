2c60e897eddf628c9dc53b1860152f566c8251e5
Wings-poo <48194169+Wings-poo@users.noreply.github.com>
2019-12-02 00:15:46 +0800

    main bfftest code - issue on quitting/kicking

--- bfftest.py
+++ bfftest.py
@@ -2,16 +2,15 @@ from bfftest_g import *
 from bfftest_obo import *
 from bfftest_s import *
 
-def hostrecv():
-    while (berk[rnum].state != "end"):
+def hostrecv(rnum,client):
+    while (berk[rnum].state != "end" and berk[rnum].deadz[client] == False):
         msg = client.recv(1024).decode("utf8")
         parse_input(rnum,client,msg)
     return
 
 def controller(rnum):
     global berk
-    print(berk[rnum].pnum)
-    #waitforplayers(rnum)
+    waitforplayers(rnum)
     berk[rnum].state = "allready"
 
     broadcast(rnum,"\nAll ready. Game start!") 
@@ -21,44 +20,46 @@ def controller(rnum):
     bygroup(rnum)
 
     berk[rnum].state = "obo"
-    #onebyone(rnum)
+    onebyone(rnum)
 
     broadcast(rnum,"Game end")
 
-    berk[rnum].state = "end"
     printscores(rnum,"end") 
 
     broadcast(rnum,"\nCongratulations to the highest scorer/s!")
     broadcast(rnum,"And of course, congratulations to the group who stayed, you have well kept your friendship! Until next time!") 
+
+    time.sleep(5.0)
+    berk[rnum].state = "end"
     return
 
 def play(room, client):
-    if (room in roomnum): # (check which room to change to room.password in roomnum) addplayer
-        rnum = roomnum[room]
-        clientroom[client] = rnum
-        berk[rnum].addplayer(client,room.user[client])#(room.names, room.user)
-    else: # move room class to sleepover class
+    if (room.names[0] == client): # assume host will not leave before game starts...
+        singlecast(client,rules)
         newroom = sleepover(room.names, room.user)#(room.names, room.user)
-        roomnum[room] = len(berk)
-        rnum = roomnum[room]
+        roomnum[room.password] = len(berk)
+        rnum = roomnum[room.password]
         clientroom[client] = rnum
         berk.append(newroom)
-
-    broadcast(rnum,"A new player appeared! Please welcome " + berk[rnum].conntoname[client] + "!\n")
-    singlecast(client,rules)
-    
-    if (room.names[0] == client): # assume host will not leave before game starts
-        controller(rnum)
-        """
+        
         ctrlr_thread = Thread(target = controller,args=(rnum,))
-        host_thread = Thread(target = hostrecv)
+        host_thread = Thread(target = hostrecv,args=(rnum,client,))
         host_thread.daemon = True
         host_thread.start()
         ctrlr_thread.start()
         ctrlr_thread.join()
-        """
 
     else:
+        while (not (room.password in roomnum)):
+            pass
+        rnum = roomnum[room.password]
+        clientroom[client] = rnum
+        if (not (client in berk[rnum].conntoname)):
+            berk[rnum].addplayer(client,room.user[client])
+
+        broadcast(rnum,"A new player appeared! Please welcome " + berk[rnum].conntoname[client] + "!\n")
+        singlecast(client,rules)
+
         if (berk[rnum].state == "bygroup"):
             singlecast(client,"You have entered in the middle of the game. No time to prepare. Good luck deciphering what's happening.")
             singlecast(client,"\n" + grules)
@@ -68,8 +69,7 @@ def play(room, client):
         elif (berk[rnum].state == "end"):
             singlecast(client,"\n Too late.")
 
-        while (berk[rnum].state != "end"):
-            print(berk[rnum].state)
+        while (berk[rnum].state != "end" and berk[rnum].deadz[client] == False):
             msg = client.recv(1024).decode("utf8")
             parse_input(rnum,client,msg)
 
