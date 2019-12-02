54d00494c386b2568d80cc08d7f577b8994c31e0
Wings-poo <48194169+Wings-poo@users.noreply.github.com>
2019-12-02 17:21:31 +0800

    Update bfftest_obo.py

--- bfftest_obo.py
+++ bfftest_obo.py
@@ -23,9 +23,8 @@ def onebyone(rnum):
         broadcast(rnum,"\nRound " +  str(i + 1) + ". Highlight is: " + berk[rnum].players[berk[rnum].player] + ".")
 
         for j in range(0,3):
-            broadcast(rnum,"\nQuestion " + str(j + 1) + ".")
             question,questioned = randq(questions, len(questions), questioned)
-            broadcast(rnum,questions[question])
+            broadcast(rnum,"\nQuestion " + str(j + 1) + ". " + questions[question])
             
             getanswer(rnum,30.0,False)
             broadcast(rnum,"\nAll answers are in. Last 10 seconds to change your answer.")
@@ -33,10 +32,18 @@ def onebyone(rnum):
                 
             printanswers(rnum)
 
+            if berk[rnum].deadz[berk[rnum].conn[berk[rnum].player]] == True:
+                break
             broadcast(rnum,"\nNow, 30 seconds to convince " + berk[rnum].players[berk[rnum].player] + " to grant you a point!")
             grantpts(rnum)
             
             printgranted(rnum)
             printscores(rnum,"ingame")
 
+            if berk[rnum].currpnum == 1:
+                break
+
             time.sleep(5.0)
+
+        if berk[rnum].currpnum == 1:
+            break
